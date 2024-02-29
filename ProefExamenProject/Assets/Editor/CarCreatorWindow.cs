6c4c0c8bfb4bc173f080d669248700712bff9b83
Sherk-cell <70703520+Sherk-cell@users.noreply.github.com>
2024-03-01 00:11:58 +0100

    Created tool for faster car importation

---
+++ ProefExamenProject/Assets/Editor/CarCreatorWindow.cs
@@ -0,0 +1,49 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CarCreatorWindow : EditorWindow
+{
+    private string name;
+    [SerializeField] private GameObject model;
+    private int topSpeed;
+    private int grip;
+    private int acceleration;
+    private int cost;
+    
+    [MenuItem("Tools/Car Creator")]
+    public static void ShowWindow()
+    {
+        GetWindow<CarCreatorWindow>("Car Creator");
+        
+    }
+    
+    
+    void OnGUI()
+    {
+        name = EditorGUILayout.TextField("Name:",name);
+        topSpeed = EditorGUILayout.IntField("Base Top speed:", topSpeed);
+        grip = EditorGUILayout.IntField("Base Grip:", grip);
+        acceleration = EditorGUILayout.IntField("Base Acceleration:", acceleration);
+        cost = EditorGUILayout.IntField("Cost:", cost);
+        model = EditorGUILayout.ObjectField("Model",model, typeof(GameObject), true) as GameObject;
+        if (GUILayout.Button("Create Car"))
+        {
+            var car = new GameObject(name);
+            if (!Directory.Exists("Assets/PlayableCars"))
+                AssetDatabase.CreateFolder("Assets", "PlayableCars");
+            string localPath = "Assets/PlayableCars/" + name + ".prefab";
+            localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
+            var scriptInstance = car.AddComponent<DemocarScript>();
+            scriptInstance.acceleration = acceleration;
+            scriptInstance.topSpeed = topSpeed;
+            scriptInstance.grip = grip;
+            scriptInstance.cost = cost;
+            Instantiate(model, car.transform);
+            PrefabUtility.SaveAsPrefabAsset(car, localPath);
+            DestroyImmediate(car);
+        }
+
+
+    }
+}
---
+++ ProefExamenProject/Assets/MockupFolder/Script/DemocarScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemocarScript : MonoBehaviour
+{
+    public int topSpeed;
+    public int grip;
+    public int acceleration;
+    public int cost;
+}
